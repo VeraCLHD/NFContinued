866c33734ccb3abbdfc6c9f53090bb58e816f1a2
Vera Boteva <Boteva@stud.uni-heidelberg.de>
2017-04-24 22:11:59 +0200

    started testing dependency parsing, test crawling with password, fixing bugs in EvaluationSuitability

--- ICrawling/src/relations_identification/DependencyParserQueryRelationsExplorer.java
+++ ICrawling/src/relations_identification/DependencyParserQueryRelationsExplorer.java
@@ -1,16 +1,23 @@
 package relations_identification;
 
+import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.ling.IndexedWord;
 import edu.stanford.nlp.ling.TaggedWord;
 import edu.stanford.nlp.parser.nndep.DependencyParser;
 import edu.stanford.nlp.parser.nndep.demo.DependencyParserDemo;
 import edu.stanford.nlp.process.DocumentPreprocessor;
+import edu.stanford.nlp.semgraph.SemanticGraph;
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 import edu.stanford.nlp.trees.GrammaticalStructure;
+import edu.stanford.nlp.trees.TypedDependency;
 import edu.stanford.nlp.util.logging.Redwood;
 
 import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * This class implements the initial extraction of connections with the help of the Stanford
@@ -18,7 +25,7 @@ import java.util.List;
  * https://nlp.stanford.edu/software/nndep.shtml
  * https://github.com/stanfordnlp/CoreNLP/blob/master/src/edu/stanford/nlp/parser/nndep/demo/DependencyParserDemo.java
  * https://nlp.stanford.edu/nlp/javadoc/javanlp-3.5.0/edu/stanford/nlp/parser/nndep/DependencyParser.html
- * The tested one here should be with the universal dependencies:
+ * The tested one here is the one with the universal dependencies:
  * edu/stanford/nlp/models/parser/nndep/english_UD.gz (default, English, Universal Dependencies)
  * http://universaldependencies.org/
  * @author Vera
@@ -56,7 +63,12 @@ public class DependencyParserQueryRelationsExplorer extends QueryRelationsExplor
 	    }
 
 	    String text = "Obesity is up, exercise is down, and the number of people eating just five servings of fruits and veggies a day dropped like a rock.";
-
+	    Map<String, String> test_terms = new HashMap<String, String>();
+	    test_terms.put("exercise", "exercise");
+	    test_terms.put("fruits", "fruit");
+	    test_terms.put("vegetables", "vegetable");
+	    	
+	    
 	    MaxentTagger tagger = new MaxentTagger(taggerPath);
 	    DependencyParser parser = DependencyParser.loadFromModelFile(modelPath);
 
@@ -65,11 +77,56 @@ public class DependencyParserQueryRelationsExplorer extends QueryRelationsExplor
 	      List<TaggedWord> tagged = tagger.tagSentence(sentence);
 	      GrammaticalStructure gs = parser.predict(tagged);
 	      // The grammatical structure has a toString()-method.
+	    
+			
+	      /*for(TypedDependency typed: gs.allTypedDependencies()){
+	    	  System.out.println(typed);
+	    	  System.out.println(typed.reln());
+	    	  System.out.println(typed.dep());
+	    	  System.out.println(typed.gov());
+	      }*/
+	      
+	      SemanticGraph graph = new SemanticGraph(gs.typedDependenciesEnhanced());
+			CoreLabel label = new CoreLabel();
+			label.setWord("exercise");
+			
+			String term2 = "fruits";
+			CoreLabel label2 = new CoreLabel();
+			label2.setWord(term2);
+			IndexedWord i = new IndexedWord(label);
+			IndexedWord i2 = new IndexedWord(label);
+			System.out.println(graph.getShortestDirectedPathNodes(i, i2));
+	      
+	      /*List<TypedDependency> tempPath = new ArrayList<TypedDependency>();
+	      for(String term1: test_terms.keySet()){
+	    	  getPathOfTerm(test_terms, gs, term1, tempPath);
+	      }*/
+	      
 	      gs.toString();
 	      System.out.println(gs);
 	      // Print typed dependencies
 	      //log.info(gs);
 	    }
+	}
+
+	private static void getPathOfTerm(Map<String, String> test_terms, GrammaticalStructure gs, String term1, List<TypedDependency> tempPath) {
+		
+		
+		for(TypedDependency typed: gs.allTypedDependencies()){
+			
+			  //which word modifies the term in question?
+			  if(typed.dep().toString().equals(term1)){
+				 
+				  tempPath.add(typed);
+				  getPathOfTerm(test_terms, gs, typed.dep().toString(), tempPath);
+				  
+			  } else if(typed.dep().lemma().equals(term1)){
+				  //which word modifies the word from if
+				  tempPath.add(typed);
+				  getPathOfTerm(test_terms, gs, typed.gov().toString(), tempPath);
+				  
+			  }
+		  }
 	}
 
 }
--- ICrawling/src/testing/EvaluationSuitability.java
+++ ICrawling/src/testing/EvaluationSuitability.java
@@ -1,9 +1,11 @@
 package testing;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -11,7 +13,11 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFTextStripper;
+
 import crawling_docs.DocProperties;
+import crawling_docs.PDFCrawler;
 import edu.stanford.nlp.simple.Sentence;
 import io.Reader;
 import io.Writer;
@@ -31,6 +37,11 @@ public class EvaluationSuitability {
 	private static Map<String,String> unusedTerms = new HashMap<String, String>();
 	private static Map<String,String> usedTerms = new HashMap<String, String>();
 	private static StanfordLemmatizer lemm = new StanfordLemmatizer();
+	
+	private static Map<String, Integer> termCount = new HashMap<String, Integer>();
+	
+	private static final String PATH_TO_NOT_DIE = "evaluation/How Not to Die.pdf";
+	
 	
 	/**
 	 * This class checks if all used (ggf. unused) terms can be found in all doc texts. 
@@ -80,21 +91,38 @@ public class EvaluationSuitability {
 		return allRawDocTexts;
 	}
 	
+	public String readPDFText(String file) throws IOException{
+		// Lowercase doctects with punctuation
+		String allRawDocTexts = Reader.readContentOfFile(file);
+		
+		return allRawDocTexts;
+	}
+	
 	public static void checkDocDumpForMatches(String allDocsStr, String term, List<Sentence> list){
-		int count = 0;
+		
 		if(term.contains(" ")){
 			Pattern r = Pattern.compile(term);
 		    Matcher m = r.matcher(allDocsStr);
 		    
 		    
 	        while (m.find()){
-	        	count+=1;
+	        	Integer count = EvaluationSuitability.getTermCount().get(term);
+	        	if(count != null){
+	        		EvaluationSuitability.getTermCount().put(term, count+1);
+	        	}
+	        	EvaluationSuitability.getTermCount().put(term, 1);
 	        }
 		} else{
 
 			for(Sentence sentence: list){
 				String t = EvaluationSuitability.getUsedTerms().get(term);
-				count += Collections.frequency(sentence.lemmas(), t);
+				Integer count = EvaluationSuitability.getTermCount().get(term);
+				int counts = Collections.frequency(sentence.lemmas(), t);
+				if(count != null){
+					EvaluationSuitability.getTermCount().put(term, count + counts);
+				}
+				
+				EvaluationSuitability.getTermCount().put(term, 0 + counts);
 			}
 			
 			/*Pattern r = Pattern.compile(EvaluationSuitability.getUsedTerms().get(term));
@@ -106,12 +134,12 @@ public class EvaluationSuitability {
 	        
 		}
 		
-		 Writer.appendLineToFile(term + "\t" + count, "evaluation_suitability.txt");
+		
 		 
-		if(count >=1){
+		/*if(finalC >=1){
 			usedInEvaluation +=1;
 			
-		}
+		}*/
             
 	    
 		/*Solution fot relations
@@ -135,24 +163,72 @@ public class EvaluationSuitability {
 	
 
 	public static void main(String[] args) {
+		//EvaluationSuitability.readPDFIntoFile(PATH_TO_NOT_DIE);
 		Writer.overwriteFile("", "evaluation_suitability.txt");
-		EvaluationSuitability eval = new EvaluationSuitability();
-		try {
-			String texts = eval.readDocDump(DocProperties.DOC_DUMP_PATH);
-			List<Sentence> sent = splitDocsInSentences(texts);
-			for(String termx: usedTerms.keySet()){
-				checkDocDumpForMatches( texts, termx, sent);
-			}
-			Writer.appendLineToFile("USED_TERMS" + "\t" + used, "evaluation_suitability.txt");
-			Writer.appendLineToFile("USED_TERMS_EVAL" + "\t" + usedInEvaluation, "evaluation_suitability.txt");
-			if(usedInEvaluation >0){
-				System.out.println(usedInEvaluation/used);
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
+			// Here, the docdump is evaluated.
+			/*String texts = eval.readDocDump(DocProperties.DOC_DUMP_PATH);
+			List<String> lines = Reader.readLinesList(DocProperties.DOC_DUMP_PATH);
+			String texts = eval.readDocDump(DocProperties.DOC_DUMP_PATH);*/
+			//Here, the pdf
+			String texts = Reader.readContentOfFile("pdf_test.txt");
+			System.out.println("Read PDF text");
+			List<String> lines = Reader.readLinesList("pdf_test.txt");
+			System.out.println("read lines");
+			evaluateSource(texts, lines);
+			
 
+	}
+
+	private static void evaluateSource(String texts, List<String> lines) {
+		int numterms = 0;
+		for(String line: lines){
+			numterms +=1;
+			System.out.println(numterms);
+		
+				for(String termx: usedTerms.keySet()){
+					List<Sentence> sent = splitDocsInSentences(line);
+					checkDocDumpForMatches( texts, termx, sent);
+			}
+		}
+		
+		/*if(usedInEvaluation >0){
+			System.out.println(usedInEvaluation/used);
+		}*/
+		// final count for a all documents
+		for(String termC: EvaluationSuitability.getTermCount().keySet()){
+			Integer finalC = EvaluationSuitability.getTermCount().get(termC);
+			Writer.appendLineToFile(termC + "\t" + finalC, "evaluation_suitability.txt");
+		}
+		
+		Writer.appendLineToFile("USED_TERMS" + "\t" + used, "evaluation_suitability.txt");
+		Writer.appendLineToFile("USED_TERMS_EVAL" + "\t" + usedInEvaluation, "evaluation_suitability.txt");
+	}
+	
+	public static String readPDFIntoFile(String path){
+			String text = "";
+			PDDocument pdf;
+			
+			try {
+				pdf = PDDocument.load(path);
+				if (pdf.isEncrypted()) {
+			        try {
+			        	pdf.decrypt("");
+			        	pdf.setAllSecurityToBeRemoved(true);
+			        }
+			        catch (Exception e) {
+			            System.err.println("The document is encrypted, and we can't decrypt it.");
+			        }
+			    }
+				PDFTextStripper pdf_stripper = new PDFTextStripper();		
+				text += pdf_stripper.getText(pdf);
+				pdf.close();
+			} catch (IOException e) {
+				 System.err.println("IOException");
+			}
+			Writer.writeEmptyFile("pdf_test.txt");
+			Writer.overwriteFile(text, "pdf_test.txt");
+			return text;
 	}
 
 	public static Map<String,String> getUnusedTerms() {
@@ -177,6 +253,14 @@ public class EvaluationSuitability {
 
 	public static void setLemm(StanfordLemmatizer lemm) {
 		EvaluationSuitability.lemm = lemm;
+	}
+
+	public static Map<String, Integer> getTermCount() {
+		return termCount;
+	}
+
+	public static void setTermCount(Map<String, Integer> termCount) {
+		EvaluationSuitability.termCount = termCount;
 	}
 
 }
